4214cb150b7e10ff6433a2d55a40eca8b9245def
g0d1ess <666funnybird666@mail.ru>
2020-08-20 19:25:13 +0400

    Fix Reset Password bugs

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,29 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordResetView, PasswordResetDoneView
 
 from .views import *
 
 app_name = 'bboard'
 
 urlpatterns = [
-
-    path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout'),
-    path('accounts/change_password/',
-         PasswordChangeView.as_view(
-             template_name='registration/change_password.html'),
-         name='password_change'),
-    path('accounts/reset_password/',
-         PasswordResetView.as_view(
-             template_name='registration/reset_password.html',
-             subject_template_name='registration/mail/reset_subject.txt',
-             email_template_name='registration/mail/reset_email.txt'),
-         name='reset_password'),
-    path('accounts/reset_password/done',
-        PasswordResetDoneView.as_view(
-            template_name='registration/password_reset_done.html'),
-        name='password_reset_done'),
-
     path('', index, name='index'),
     path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
--- learn/urls.py
+++ learn/urls.py
@@ -1,10 +1,41 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView, \
+    PasswordChangeView, PasswordResetView, PasswordResetDoneView, \
+    PasswordResetConfirmView, PasswordResetCompleteView
+
 
 from bboard.views import index
 
 urlpatterns = [
     path('', include('bboard.urls'), name=''),
     path('admin/', admin.site.urls),
+
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+    path('accounts/change_password/',
+         PasswordChangeView.as_view(
+             template_name='registration/change_password.html'),
+         name='password_change'),
+    path('accounts/reset_password/',
+         PasswordResetView.as_view(
+             template_name='registration/reset_password.html',
+             subject_template_name='registration/mail/reset_subject.txt',
+             email_template_name='registration/mail/reset_email.txt'),
+         name='reset_password'),
+    path('accounts/reset/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='registration/confirm_password.html'),
+         name='password_reset_confirm'),
+    path('accounts/reset_password/done',
+         PasswordResetDoneView.as_view(
+             template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+    path('accounts/reset/done',
+         PasswordResetCompleteView.as_view(
+             template_name='registration/password_confirmed.html'
+         ),
+         name='password_reset_complete'),
+
 ]
