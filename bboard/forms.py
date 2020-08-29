ee2181b04338cd82da3ea717ad81b9f4da8daefe
g0d1ess <666funnybird666@mail.ru>
2020-08-29 16:07:24 +0400

    Fix Bboard form and Bboard model

--- bboard/forms.py
+++ bboard/forms.py
@@ -8,7 +8,7 @@ class BbForm(forms.ModelForm):
     image = forms.ImageField(label='Изображение',
                              validators=[validators.FileExtensionValidator(
                                  allowed_extensions=('gif', 'jpg', 'png'))],
-                             error_messages={'invalid_extension': 'Этот формат файлов ' + \
+                             error_messages={'invalid_extension': 'Этот формат файлов ' +
                                                                   'не поддерживается'})
 
     class Meta:
@@ -20,7 +20,7 @@ class ImgForm(forms.ModelForm):
     img = forms.ImageField(label='Изображение',
                            validators=[validators.FileExtensionValidator(
                                allowed_extensions=('gif', 'jpg', 'png'))],
-                           error_messages={'invalid_extension': 'Этот формат файлов ' + \
+                           error_messages={'invalid_extension': 'Этот формат файлов ' +
                                                                 'не поддерживается'})
     desc = forms.CharField(label='Описание',
                            widget=forms.widgets.Textarea())
--- bboard/models.py
+++ bboard/models.py
@@ -34,11 +34,15 @@ class Rubric(models.Model):
 
 class Bb(models.Model):
     title = models.CharField(max_length=50, verbose_name='Товар')
-    image = models.ImageField(null=True, verbose_name='Изображение')
+    image = models.ImageField(null=True, verbose_name='Изображение', upload_to=get_timestamp_path)
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+
+    def delete(self, *args, **kwargs):
+        self.image.delete(save=False)
+        super().delete(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Обьявления'
